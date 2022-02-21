369942f6a098966c6f61570b8a233a97d1aa71c3
SimonFalkner <76668461+SimonFalkner@users.noreply.github.com>
2022-02-21 14:19:24 +0100

    Added mapbox in mainActivity.java and fragment_map.xml

--- app/src/main/java/com/example/seite/MainActivity.java
+++ app/src/main/java/com/example/seite/MainActivity.java
@@ -10,10 +10,15 @@ import android.view.MenuItem;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private MapView mapView;
     MaterialToolbar topAppBar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -21,11 +26,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
         setContentView(R.layout.activity_main);
 
         topAppBar = findViewById(R.id.topAppBar);
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigationView);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                    }
+                });
+            }
+        });
+
         //bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         topAppBar.setNavigationOnClickListener(view -> drawerLayout.open());
